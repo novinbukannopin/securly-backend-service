5ea21be316603f5cdf798abbe3a4d8486fbed317
novinbukannopin <novinardiany@outlook.com>
2025-02-10 21:35:16 +0700

    err: debug secret

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testEnvironmentOptions: {
-    NODE_ENV: 'test'
+    NODE_ENV: 'development'
   },
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
